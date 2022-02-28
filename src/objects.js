dc616ad03dc0d39adb761371045a939a1a04492d
Thomas <thomas.bogle@outlook.com>
2022-02-28 20:07:10 +0000

    Finished half of the object katas

--- src/objects.js
+++ src/objects.js
@@ -1,25 +1,34 @@
 const createPerson = (name, age) => {
-  // your code here
+  return {
+    'name': name,
+    'age': age 
+  };
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  if (object[property]) {
+    return true;
+  }
+  return false;
 };
 
 const isOver65 = person => {
-  // your code here
+  if (person.age > 65) {
+    return true;
+  }
+  return false;
 };
 
 const getAges = people => {
-  // your code here
+  // return people.forEach(people.age); //
 };
 
 const findByName = (name, people) => {
