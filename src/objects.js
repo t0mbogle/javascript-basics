49c883a50bc462eabfed512b79000dbcbd385b14
Thomas <thomas.bogle@outlook.com>
2022-03-01 14:47:32 +0000

    Completed all object katas

--- src/objects.js
+++ src/objects.js
@@ -28,23 +28,40 @@ const isOver65 = person => {
 };
 
 const getAges = people => {
-  // return people.forEach(people.age); //
+  return people.map(index => index.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.filter(index => index.name === name)[0];
 };
 
 const findHondas = cars => {
-  // your code here
+  let hondaCars = [];
+  for (const i in cars) {
+    if (cars[i].manufacturer === 'Honda') {
+      hondaCars.push(cars[i]);
+    }
+  }
+  return hondaCars;
 };
 
 const averageAge = people => {
-  // your code here
+  let avrAge = 0;
+  for (const i in people) {
+    avrAge += Math.round(people[i].age / people.length);
+  }
+  return avrAge;
+  // Could try using .reduce() as another method of getting one number from mulitple //
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  return {
+    'name': name,
+    'age': age,
+    'introduce': function(name2) {
+      return `Hi ${name2}, my name is ${this.name} and I am ${this.age}!`;
+    }
+  };
 };
 
 module.exports = {
